dd7b23bd86fdbab2a668cd8e975840c54b0c001d
mangesh9903 <mangeshb9903@gmail.com>
2022-03-07 08:53:56 +0530

    UC 1 commit

---
+++ src/main/java/com/bridgelabz/gamblergame/Gambler.java
@@ -0,0 +1,11 @@
+package com.bridgelabz.gamblergame;
+
+public class Gambler {
+
+   public static final int STAKE=100;
+   public static final int BET=1;
+    public static void main(String[] args) {
+
+        System.out.println("*********** Welcome to Gambler Game *******" );
+    }
+}
