1c96e43b2fe3ce66c2a244bfbd4ab124f1661847
mangesh9903 <mangeshb9903@gmail.com>
2022-03-07 09:15:29 +0530

    UC 2 commit

--- src/main/java/com/bridgelabz/gamblergame/Gambler.java
+++ src/main/java/com/bridgelabz/gamblergame/Gambler.java
@@ -2,10 +2,25 @@ package com.bridgelabz.gamblergame;
 
 public class Gambler {
 
-   public static final int STAKE=100;
-   public static final int BET=1;
+    public static final int STAKE = 100;
+    public static final int BET = 1;
+
     public static void main(String[] args) {
 
-        System.out.println("*********** Welcome to Gambler Game *******" );
+        Gambler gambler = new Gambler();
+        System.out.println("******* Welcome to Gambler Game *******");
+        gambler.checkWinOrLose();
+        System.out.println("*********** Gambler Game Finished *******");
+
+    }
+
+    public void checkWinOrLose() {
+        boolean randomlyCheck = (Math.random() < 0.5);
+
+        if (randomlyCheck) {
+            System.out.println(" Congrats Player Win ");
+        } else {
+            System.out.println(" Player Lose ");
+        }
     }
 }
